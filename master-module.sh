b0c738c9be1f75145345839421f66edd42e65e98
Janne <bhi256@myy.haaga-helia.fi>
2025-05-03 16:02:22 +0300

    Added module for web servers

--- master-module.sh
+++ master-module.sh
@@ -1,5 +1,6 @@
 ssh-keygen -q -t rsa -N "" -f ../.ssh/id_rsa <<< y
 sudo mkdir /srv/salt
 sudo cp -a ./admin /srv/salt/admin
+sudo cp -a ./web /srv/salt/web
 sudo cp -a ../.ssh/id_rsa.pub /srv/salt/admin/id_rsa.pub
 sudo cp -a ./top.sls /srv/salt/top.sls
---
+++ top.sls
@@ -0,0 +1,5 @@
+base:
+  '*':
+    - admin
+  'slave*'
+    - web
