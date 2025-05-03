fb0e8da482e5fe350d0f6fa35d666cbfa78557aa
Janne <bhi256@myy.haaga-helia.fi>
2025-05-03 11:40:37 +0300

    Minor fixes to paths in initial setup

--- master.sh
+++ master.sh
@@ -4,8 +4,7 @@ curl -fsSL https://packages.broadcom.com/artifactory/api/security/keypair/SaltPr
 curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/download/salt.sources | sudo tee /etc/apt/sources.list.d/salt.sources
 sudo apt-get update
 sudo apt-get install salt-master -y
-sudo apt-get install git -y
-ssh-keygen -q -t rsa -N "" -f ~/.ssh/id_rsa <<< y
+ssh-keygen -q -t rsa -N "" -f ../.ssh/id_rsa <<< y
 sudo mkdir /srv/salt
-cp ~/.ssh/id_rsa.pub ~/salt-admin-setup/admin/id_rsa.pub
-sudo cp -r -p ~/salt-admin-setup/admin /srv/salt/admin
+cp ../.ssh/id_rsa.pub /admin/id_rsa.pub
+sudo cp -r -p /admin /srv/salt/admin
--- slave.sh
+++ slave.sh
@@ -4,6 +4,5 @@ curl -fsSL https://packages.broadcom.com/artifactory/api/security/keypair/SaltPr
 curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/download/salt.sources | sudo tee /etc/apt/sources.list.d/salt.sources
 sudo apt-get update
 sudo apt-get install salt-minion -y
-echo "master: 192.168.2.10" | sudo tee -a /etc/salt/minion
+echo "master: 192.168.2.10" | sudo tee -a /etc/salt/minion #update master IP
 sudo systemctl restart salt-minion
-sudo apt-get install git -y
