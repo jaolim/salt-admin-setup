1674e2d1c2b52aa8a54237b6552d26ff64975660
Janne <bhi256@myy.haaga-helia.fi>
2025-05-09 14:22:24 +0300

    Modified salt-minions to re-establish connection to master faster since the active connection is lost on ufw enable

--- slave.sh
+++ slave.sh
@@ -6,4 +6,6 @@ curl -fsSL https://github.com/saltstack/salt-install-guide/releases/latest/downl
 sudo apt-get update
 sudo apt-get install salt-minion -y
 echo "master: 192.168.2.10" | sudo tee -a /etc/salt/minion #update master IP
+echo "tcp_keepalive: True" | sudo tee -a /etc/salt/minion #workaround for lost connection on ufw enable
+echo "tcp_keepalive_idle: 10" | sudo tee -a /etc/salt/minion #workaround for lost connection on ufw enable
 sudo systemctl restart salt-minion
